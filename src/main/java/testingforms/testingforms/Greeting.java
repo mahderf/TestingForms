c1073d8f13bac3b5087bd6aa7baa33b0f123141a
mahderf <mymahder@gmail.com>
2017-08-02 14:12:06 -0400

    adding files

---
+++ src/main/java/testingforms/testingforms/Greeting.java
@@ -0,0 +1,25 @@
+package testingforms.testingforms;
+
+public class Greeting {
+    private long id;
+    private String content;
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+    public String getContent() {
+        return content;
+    }
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
